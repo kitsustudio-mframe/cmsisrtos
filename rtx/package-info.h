a109da9be55ee8a4f5a362193ce5b15f4450fb95
ZxyKira <danny1191365@kitsu-studio.net>
2023-06-11 23:35:16 +0800

    [add] package-info

---
+++ rtx/package-info.h
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2020 ZxyKira
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+#ifndef CMSISRTOS_D215E5A1_B0AB_40DA_86A1_7D036C9356A6
+#define CMSISRTOS_D215E5A1_B0AB_40DA_86A1_7D036C9356A6
+
+/* ****************************************************************************************
+ * Include folder
+ */
+
+/* ****************************************************************************************
+ * Include
+ */
+#ifdef USING_CMSISRTOS_RTX_OS
+#include "./rtx_os.h"
+#endif
+
+#ifdef USING_CMSISRTOS_RTX_LIB
+#include "./rtx_lib.h"
+#endif
+
+#ifdef USING_CMSISRTOS_RTX_EVR
+#include "./rtx_evr.h"
+#endif
+
+#ifdef USING_CMSISRTOS_RTX_DEF
+#include "./rtx_def.h"
+#endif
+
+#ifdef USING_CMSISRTOS_OS2
+#include "./cmsis_os2.h"
+#endif
+
+/* ****************************************************************************************
+ * End of file
+ */
+
+#endif /* CMSISRTOS_D215E5A1_B0AB_40DA_86A1_7D036C9356A6 */
